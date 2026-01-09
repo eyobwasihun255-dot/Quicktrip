e1a8f76295e05ac9522691e8f13151bffcfe5dec
eyobwasihun255-dot <eyobwasihun255@gmail.com>
2026-01-09 22:20:56 +0300

    Update payment.jsx

--- frontend/src/pages/payment.jsx
+++ frontend/src/pages/payment.jsx
@@ -65,7 +65,7 @@ const [loadings, setLoadings] = useState(false);
     try {
      
       
-      const response = await fetch('http://127.0.0.1:8000/api/payment/', {
+      const response = await fetch('https://quicktrip-e761.onrender.com/api/payment/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ amount: 500.00 }),
