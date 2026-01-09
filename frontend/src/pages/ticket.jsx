95b79210e0846483f5d9c72b238985eca74ae12c
eyobwasihun255-dot <eyobwasihun255@gmail.com>
2026-01-09 22:21:23 +0300

    Update ticket.jsx

--- frontend/src/pages/ticket.jsx
+++ frontend/src/pages/ticket.jsx
@@ -57,7 +57,7 @@ export default function TicketPurchase() {
     setLoadings(true);
     try {
      
-      const response = await fetch('http://127.0.0.1:8000/api/payment/', {
+      const response = await fetch('https://quicktrip-e761.onrender.com/api/payment/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ amount: 500.00 }),
